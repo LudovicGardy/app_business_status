00f5d60d6f899797aaa2f434295d1595ac0cb456
Ludovic Gardy <ludovic.gardy@gmail.com>
2024-07-23 22:24:32 +0200

    corrected syntax in setup.sh

--- setup.sh
+++ setup.sh
@@ -7,8 +7,8 @@ enableCORS = false\n\
 \n\
 [theme]\n\
 base = \"dark\"\n\
-layout="centered"
-initial_sidebar_state="expanded"
-page_title="Business Status"
-sidebar_title="Business Status"
+layout=\"centered\"\n\
+initial_sidebar_state=\"expanded\"\n\
+page_title=\"Business Status\"\n\
+sidebar_title=\"Business Status\"\n\
 " > ~/.streamlit/config.toml
